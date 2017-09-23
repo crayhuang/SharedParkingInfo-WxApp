5b8103b17f54704afc05e1170b54ac81674b2cfa
Cray <cray.huang@gmail.com>
2017-09-23 22:32:07 +0800

    implemented log user info function

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,48 @@ var timestamp = Date.parse(new Date()) / 1000;
 
 Page({
   onLoad: function () {
+    wx.getUserInfo({
+      success: function(res){
+        var user_info = res.userInfo
+        var nick_name = user_info.nickName
+        var avatar_url = user_info.avatarUrl
+        var gender = user_info.gender
+        var province = user_info.province
+        var city = user_info.city
+        var country = user_info.country
+
+        const params = {}
+        params['nick_name'] = nick_name
+        params['avatar_url'] = avatar_url
+        params['gender'] = gender
+        params['province'] = province
+        params['city'] = city
+        params['country'] = country
+        
+        console.log("User Info: " + user_info.nickName)
+        wx.request({
+          url: app.API_URL + 'api/v1.0/user_info',
+          data: params,
+          method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+          // header: {}, // 设置请求的 header
+          success: function(res){
+            console.log("log user info successfully!")
+          },
+          fail: function() {
+            // fail
+          },
+          complete: function() {
+            // complete
+          }
+        })
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
 		$wuxLoading.show({
 			text: '数据加载中'
 		})
