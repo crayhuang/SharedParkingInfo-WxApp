848f39ad217b6a9251769f77122b795232f214f3
Cray <cray.huang@gmail.com>
2018-01-15 11:55:35 +0800

    implemented pull down refresh and search with empty string

--- pages/index/index.js
+++ pages/index/index.js
@@ -49,81 +49,94 @@ Page({
         // complete
       }
     })
-		$wuxLoading.show({
-			text: '数据加载中'
-		})
-		this.button = $wuxButton.init('br')
-    var cur_latitude
-    var cur_longitude
-
-    var that = this
-    wx.getLocation({
-      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
-      success: function (res) {
-        cur_latitude = res.latitude
-        cur_longitude = res.longitude
-      },
-      fail: function () {
-        // fail
-      },
-      complete: function () {
-        wx.request({
-          // url: app.API_URL + 'api/v1.0/parking_infos?latitude=' + cur_latitude + '&longitude=' + cur_longitude,
-          url: app.API_URL + 'api/v1.0/parking_infos',
-          data: {
-            latitude: cur_latitude,
-            longitude: cur_longitude,
-            timestamp: timestamp,
-            sign: crypto.SHA1(timestamp + app.APP_ID + app.APP_SECRET).toString()
-          },
-          method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-          // header: {}, // 设置请求的 header
-          success: (res) => {
-            that.setData({
-              parking_info_list: res.data.object,
-              icon20: base64.icon20,
-              icon60: base64.icon60
-						})
-						$wuxLoading.hide()
-          },
-          fail: function () {
-            // fail
-          },
-          complete: function () {
-            // complete
-          }
-				})
-      }
-    })
+    getParkingInfoList(this)
   },
   search: function(e) {
-    $wuxLoading.show({
-			text: '数据加载中'
-		})
-    var that = this
-    wx.request({
-      url: app.API_URL + 'api/v1.0/parking_infos/search',
-      data: {
-        keyword: e.detail.value,
-        timestamp: timestamp,
-        sign: crypto.SHA1(timestamp + app.APP_ID + app.APP_SECRET).toString()
-      },
-      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      // header: {}, // 设置请求的 header
-      success: function(res){
-        that.setData({
-          parking_info_list: res.data.object,
-          icon20: base64.icon20,
-          icon60: base64.icon60
-        })
-        $wuxLoading.hide()
-      },
-      fail: function() {
-        // fail
-      },
-      complete: function() {
-        // complete
-      }
-    })
+    if (e.detail.value == null || e.detail.value == "") {
+      getParkingInfoList(this)
+    }
+    else {
+      $wuxLoading.show({
+        text: '数据加载中'
+      })
+      var that = this
+      wx.request({
+        url: app.API_URL + 'api/v1.0/parking_infos/search',
+        data: {
+          keyword: e.detail.value,
+          timestamp: timestamp,
+          sign: crypto.SHA1(timestamp + app.APP_ID + app.APP_SECRET).toString()
+        },
+        method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+        // header: {}, // 设置请求的 header
+        success: function(res){
+          that.setData({
+            parking_info_list: res.data.object,
+            icon20: base64.icon20,
+            icon60: base64.icon60
+          })
+          $wuxLoading.hide()
+        },
+        fail: function() {
+          // fail
+        },
+        complete: function() {
+          // complete
+        }
+      })
+    }
+  },
+  onPullDownRefresh: function() {
+    console.log("pull down refresh")
+    getParkingInfoList(this)
   }
 })
+
+function getParkingInfoList(e) {
+  $wuxLoading.show({
+    text: '数据加载中'
+  })
+  var cur_latitude
+  var cur_longitude
+
+  var that = e
+  wx.getLocation({
+    type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+    success: function (res) {
+      cur_latitude = res.latitude
+      cur_longitude = res.longitude
+    },
+    fail: function () {
+      // fail
+    },
+    complete: function () {
+      wx.request({
+        // url: app.API_URL + 'api/v1.0/parking_infos?latitude=' + cur_latitude + '&longitude=' + cur_longitude,
+        url: app.API_URL + 'api/v1.0/parking_infos',
+        data: {
+          latitude: cur_latitude,
+          longitude: cur_longitude,
+          timestamp: timestamp,
+          sign: crypto.SHA1(timestamp + app.APP_ID + app.APP_SECRET).toString()
+        },
+        method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+        // header: {}, // 设置请求的 header
+        success: (res) => {
+          that.setData({
+            parking_info_list: res.data.object,
+            icon20: base64.icon20,
+            icon60: base64.icon60
+          })
+          $wuxLoading.hide()
+          wx.stopPullDownRefresh()
+        },
+        fail: function () {
+          // fail
+        },
+        complete: function () {
+          // complete
+        }
+      })
+    }
+  })
+}
